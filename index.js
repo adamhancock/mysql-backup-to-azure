0d8857fb6aa9347c4cfe0ab54ff6d10262b0d525
adammhancock@gmail.com <adammhancock@gmail.com>
2020-08-05 11:23:18 +0100

    added comments

--- index.js
+++ index.js
@@ -3,15 +3,20 @@ const listDatabases = require('./listMySQLDatabases')
 const moment = require('moment')
 const backupMySQL = require('./backupMySQL')
 ;(async function () {
+  // Build array of database names.
   const databases = await listDatabases()
+
+  // for each database
   for await (const db of databases) {
     const fileName = `${moment().format('DD-MM-YY')}/${db}_${moment().format(
       'HH-mm'
     )}.sql`
     console.log(`Backing up ${db}`)
+    // backup database
     const content = await backupMySQL(db)
-
+    // check MySQL dump is not empty
     if (content != undefined) {
+      // Backup to azure
       await backupToAzure(fileName, content.dump.data)
     }
   }
